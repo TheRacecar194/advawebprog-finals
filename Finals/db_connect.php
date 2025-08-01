8b89be7cc117d55af97f7303c09600c4b2aec5f9
TheRacecar194 <82567791+TheRacecar194@users.noreply.github.com>
2025-08-01 14:40:05 +0800

    Rename files; Create connection files.

---
+++ Finals/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+//connection credentials + name of database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "school_db";
+
+//connection command
+$conn = new mysqli($servername, $username, $password, $database)
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error); //check if connection fails
+}
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include 'db_connect.php' //establishes connection to mysql database
+
+//create students table
+$sql = "CREATE TABLE students(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        full_name VARCHAR(100),
+        dob DATE,
+        gender ENUM('Male','Female','Other'),
+        course VARCHAR(50),
+        year_level INT,
+        contact_number VARCHAR(15),
+        email VARCHAR(100),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );"
+
+?>
