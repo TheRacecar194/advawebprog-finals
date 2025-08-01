08e16cd47563c05a4f8bd0bb3c549b5ae7473c56
TheRacecar194 <82567791+TheRacecar194@users.noreply.github.com>
2025-08-01 16:31:13 +0800

    Small update

--- display.php
+++ display.php
@@ -6,6 +6,7 @@ $sql = "SELECT * FROM student";
 $result = $conn->query($sql);
 
 //display all data in loop
+echo '<h3>Student Data:<>h3'
 while($row = $result->fetch_assoc()){
     echo "Student ID: {$row['id']} - Full Name: {$row['full_name']} - Date of Birth: {$row['dob']} - Gender: {$row['gender']} - Course: {$row['course']} - Year Level: {$row['year_level']} - Contact No.: {$row['contact_number']} - Email: {$row['email']}";
 }
