3732730174e2f0c6f6d43b3a6cb1243923a9e844
TheRacecar194 <82567791+TheRacecar194@users.noreply.github.com>
2025-08-01 16:32:47 +0800

    Fixed further oversight on display.php

--- display.php
+++ display.php
@@ -6,9 +6,9 @@ $sql = "SELECT * FROM student";
 $result = $conn->query($sql);
 
 //display all data in loop
-echo '<h3>Student Data:<>h3'
+echo '<h3>Student Data:<h3>';
 while($row = $result->fetch_assoc()){
-    echo "Student ID: {$row['id']} - Full Name: {$row['full_name']} - Date of Birth: {$row['dob']} - Gender: {$row['gender']} - Course: {$row['course']} - Year Level: {$row['year_level']} - Contact No.: {$row['contact_number']} - Email: {$row['email']}";
+    echo "Student ID: {$row['id']} - Full Name: {$row['full_name']} - Date of Birth: {$row['dob']} - Gender: {$row['gender']} - Course: {$row['course']} - Year Level: {$row['year_level']} - Contact No.: {$row['contact_number']} - Email: {$row['email']}<br>";
 }
 $conn->close(); //closes db connection
 ?>
