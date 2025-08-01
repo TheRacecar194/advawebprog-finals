face299feaf19a32159c6e05045c085a42da2d51
TheRacecar194 <82567791+TheRacecar194@users.noreply.github.com>
2025-08-01 16:39:29 +0800

    Small update

--- create_database.php
+++ create_database.php
@@ -4,7 +4,7 @@ include 'db_connect'; //establish connection to db
 //creates database with specified name
 $sql = "CREATE DATABASE school_db";
 
-//check if creation fails
+//check if creation fails, if otherwise, acknowledge db creation
 if ($conn->query($sql) === FALSE) {
     echo "Error creating database: " . $conn->error;
 }else{
