7f6cd18786070801ae60c528dfbce4dd3b6c9b37
TheRacecar194 <82567791+TheRacecar194@users.noreply.github.com>
2025-08-01 16:39:07 +0800

    Small update

--- create_database.php
+++ create_database.php
@@ -7,6 +7,8 @@ $sql = "CREATE DATABASE school_db";
 //check if creation fails
 if ($conn->query($sql) === FALSE) {
     echo "Error creating database: " . $conn->error;
+}else{
+    echo "Database Creation Successful.";
 }
 
 $conn->close(); //closes db connection
