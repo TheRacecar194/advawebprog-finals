a74d6a846dc1d8fbe9def061676719bb46827afa
TheRacecar194 <82567791+TheRacecar194@users.noreply.github.com>
2025-08-08 10:29:12 +0800

    Fixed display.php

--- display.php
+++ display.php
@@ -8,7 +8,7 @@ $result = $conn->query($sql);
 //display all data in loop
 echo '<h3>Student Data:<h3>';
 while($row = $result->fetch_assoc()){
-    echo "Student ID: {$row['id']} - Full Name: {$row['full_name']} - Date of Birth: {$row['dob']} - Gender: {$row['gender']} - Course: {$row['course']} - Year Level: {$row['year_level']} - Contact No.: {$row['contact_number']} - Email: {$row['email']}<br>";
+    echo " <br> Student ID: {$row['id']} <br> - Full Name: {$row['full_name']} <br> - Date of Birth: {$row['dob']} <br> - Gender: {$row['gender']} <br> - Course: {$row['course']} <br> - Year Level: {$row['year_level']} <br> - Contact No.: {$row['contact_number']} <br> - Email: {$row['email']}<br>";
 }
 $conn->close(); //closes db connection
 ?>
